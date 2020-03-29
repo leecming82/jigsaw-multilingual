b6502c1e4b9d5dd67cff617d0d84ae17e0c160df
leecming <leecming@gmail.com>
2020-03-29 19:41:31 +0800

    BART-specific code (e.g., resizing embeddings)

--- classifier_torch.py
+++ classifier_torch.py
@@ -17,28 +17,30 @@ from tqdm import trange
 from preprocessor import get_id_text_label_from_csv, get_id_text_distill_label_from_csv
 from torch_helpers import EMA, save_model
 
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-
-SAVE_MODEL = False
+SAVE_MODEL = True
 USE_AMP = True
 USE_EMA = False
-USE_DISTILL = False
-PRETRAINED_MODEL = 'xlm-roberta-base'
-TRAIN_CSV_PATH = 'data/jigsaw-toxic-comment-train.csv'
-DISTIL_CSV_PATH = None
-# TRAIN_CSV_PATH = 'data/toxic_2018/train.csv'
-# DISTIL_CSV_PATH = 'data/toxic_2018/ensemble_3.csv'
+USE_DISTILL = True
+USE_MULTI_GPU = False
+PRETRAINED_MODEL = 'bart-large'
+# TRAIN_CSV_PATH = 'data/jigsaw-toxic-comment-train.csv'
+# DISTIL_CSV_PATH = None
+TRAIN_CSV_PATH = 'data/toxic_2018/train.csv'
+DISTIL_CSV_PATH = 'data/toxic_2018/ensemble_3.csv'
 VAL_CSV_PATH = 'data/validation_en.csv'
 OUTPUT_DIR = 'models/'
 NUM_GPUS = 2  # Set to 1 if using AMP (doesn't seem to play nice with 1080 Ti)
 MAX_CORES = 24  # limit MP calls to use this # cores at most
-BASE_MODEL_OUTPUT_DIM = 768  # hidden layer dimensions
+BASE_MODEL_OUTPUT_DIM = 1024  # hidden layer dimensions
 INTERMEDIATE_HIDDEN_UNITS = 1
 MAX_SEQ_LEN = 200  # max sequence length for input strings: gets padded/truncated
 NUM_EPOCHS = 5
-BATCH_SIZE = 64
+BATCH_SIZE = 24
 EMA_DECAY = 0.999
+
+if not USE_MULTI_GPU:
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
 class ClassifierHead(torch.nn.Module):
@@ -51,6 +53,7 @@ class ClassifierHead(torch.nn.Module):
         super(ClassifierHead, self).__init__()
         self.base_model = base_model
         self.cnn = torch.nn.Conv1d(BASE_MODEL_OUTPUT_DIM, INTERMEDIATE_HIDDEN_UNITS, kernel_size=1)
+        self.fc = torch.nn.Linear(BASE_MODEL_OUTPUT_DIM, INTERMEDIATE_HIDDEN_UNITS)
 
     def forward(self, x, freeze=True):
         if freeze:
@@ -59,11 +62,12 @@ class ClassifierHead(torch.nn.Module):
         else:
             hidden_states = self.base_model(x)[0]
 
-        hidden_states = hidden_states.permute(0, 2, 1)
-        cnn_states = self.cnn(hidden_states)
-        cnn_states = cnn_states.permute(0, 2, 1)
-        logits, _ = torch.max(cnn_states, 1)
+        # hidden_states = hidden_states.permute(0, 2, 1)
+        # cnn_states = self.cnn(hidden_states)
+        # cnn_states = cnn_states.permute(0, 2, 1)
+        # logits, _ = torch.max(cnn_states, 1)
 
+        logits = self.fc(hidden_states[:, -1, :])
         prob = torch.nn.Sigmoid()(logits)
         return prob
 
@@ -132,10 +136,13 @@ def evaluate(model, val_tuple):
     return val_roc_auc_score
 
 
-def main_driver(train_tuple, val_raw_tuple, val_translated_tuple):
+def main_driver(train_tuple, val_raw_tuple, val_translated_tuple, tokenizer):
     pretrained_config = AutoConfig.from_pretrained(PRETRAINED_MODEL,
                                                    output_hidden_states=True)
     pretrained_base = AutoModel.from_pretrained(PRETRAINED_MODEL, config=pretrained_config).cuda()
+    new_embed = pretrained_base.resize_token_embeddings(len(tokenizer))
+    pretrained_base.encoder.embed_tokens = new_embed
+    pretrained_base.decoder.embed_tokens = new_embed
     classifier = ClassifierHead(pretrained_base).cuda()
 
     if USE_EMA:
@@ -153,6 +160,9 @@ def main_driver(train_tuple, val_raw_tuple, val_translated_tuple):
         amp.register_float_function(torch, 'sigmoid')
         classifier, opt = amp.initialize(classifier, opt, opt_level='O1', verbosity=0)
 
+    if USE_MULTI_GPU:
+        classifier = torch.nn.DataParallel(classifier)
+
     best_raw_auc, best_translated_auc = -1, -1
     list_raw_auc, list_translated_auc = [], []
     for curr_epoch in range(NUM_EPOCHS):
@@ -164,12 +174,8 @@ def main_driver(train_tuple, val_raw_tuple, val_translated_tuple):
         list_raw_auc.append(epoch_raw_auc)
         list_translated_auc.append(epoch_translated_auc)
 
-        if epoch_raw_auc > best_raw_auc and SAVE_MODEL:
-            print('Raw AUC increased; saving model')
-            best_raw_auc = epoch_raw_auc
-            save_model(os.path.join(OUTPUT_DIR, PRETRAINED_MODEL), classifier, pretrained_config, tokenizer)
-        elif epoch_translated_auc > best_translated_auc and SAVE_MODEL:
-            print('Raw AUC increased; saving model')
+        if epoch_translated_auc > best_translated_auc and SAVE_MODEL:
+            print('Translated AUC increased; saving model')
             best_translated_auc = epoch_translated_auc
             save_model(os.path.join(OUTPUT_DIR, PRETRAINED_MODEL), classifier, pretrained_config, tokenizer)
 
@@ -202,6 +208,10 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
     if 'gpt' in PRETRAINED_MODEL:  # GPT2 pre-trained tokenizer doesn't set a padding token
         tokenizer.add_special_tokens({'pad_token': '<|endoftext|>'})
+    tokenizer.add_tokens('<special>')
+
+    special_token = tokenizer.encode('<special>', add_special_tokens=False)
+    print(special_token)
 
     encode_partial = partial(tokenizer.encode,
                              max_length=MAX_SEQ_LEN,
@@ -213,10 +223,17 @@ if __name__ == '__main__':
         val_raw_features = np.array(p.map(encode_partial, val_raw_strings))
         val_translated_features = np.array(p.map(encode_partial, val_translated_strings))
 
+    train_features[:, -1] = special_token
+    val_raw_features[:, -1] = special_token
+    val_translated_features[:, -1] = special_token
+
+    print(train_features[0])
+
     print(train_features.shape, val_raw_features.shape, val_translated_features.shape)
 
     main_driver([train_features, train_labels, train_ids],
                 [val_raw_features, val_labels, val_ids],
-                [val_translated_features, val_labels, val_ids])
+                [val_translated_features, val_labels, val_ids],
+                tokenizer)
 
     print('ELapsed time: {}'.format(time.time() - start_time))
