98786da488ef09b3c7ca83d7f56ad730798812d4
leecming <leecming@gmail.com>
2020-03-29 21:09:15 +0800

    clean up prediction functionality for quality of life

--- submitter.py
+++ submitter.py
@@ -7,15 +7,18 @@ import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModel, AutoConfig, WEIGHTS_NAME
 
-TRAINED_MODEL_PATH = 'models/queue/basic bert'
+TRAINED_MODEL_PATH = 'models/bert-base-multilingual-cased'
 TEST_CSV_PATH_1 = 'data/test_en.csv'
 TEST_CSV_PATH_2 = 'data/jigsaw_miltilingual_test_translated.csv'
-SUBMIT_CSV_PATH = 'data/submission.csv'
+SUBMIT_CSV_PATH = 'data/test/bert-base-multilingual-cased.csv'
 MAX_CORES = 24
 MAX_SEQ_LEN = 200
 BATCH_SIZE = 64
 BASE_MODEL_OUTPUT_DIM = 768  # hidden layer dimensions
 INTERMEDIATE_HIDDEN_UNITS = 1
+
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def get_id_text_from_test_csv(csv_path, text_col):
@@ -33,6 +36,7 @@ class ClassifierHead(torch.nn.Module):
         super(ClassifierHead, self).__init__()
         self.base_model = base_model
         self.cnn = torch.nn.Conv1d(BASE_MODEL_OUTPUT_DIM, INTERMEDIATE_HIDDEN_UNITS, kernel_size=1)
+        self.fc = torch.nn.Linear(BASE_MODEL_OUTPUT_DIM, INTERMEDIATE_HIDDEN_UNITS)
 
     def forward(self, x, freeze=True):
         if freeze:
@@ -50,14 +54,25 @@ class ClassifierHead(torch.nn.Module):
         return prob
 
 
+def predict(model, input_features):
+    print('Predicting test comments')
+    preds = []
+    model.eval()
+    with torch.no_grad():
+        for batch_idx_start in range(0, len(input_features), BATCH_SIZE):
+            batch_idx_end = min(batch_idx_start + BATCH_SIZE, len(input_features))
+            batch_features = torch.tensor(input_features[batch_idx_start:batch_idx_end]).cuda()
+            batch_preds = model(batch_features)
+            preds.append(batch_preds.cpu())
+
+    return np.concatenate(preds).squeeze()
+
+
 start_time = time.time()
-all_ids, all_strings = get_id_text_from_test_csv(TEST_CSV_PATH_1, text_col='content_en')
+all_ids, all_strings_1 = get_id_text_from_test_csv(TEST_CSV_PATH_1, text_col='content_en')
 _, all_strings_2 = get_id_text_from_test_csv(TEST_CSV_PATH_2, text_col='translated')
 
-assert len(all_strings) == len(all_strings_2)
-
-all_strings = all_strings + all_strings_2
-print(len(all_strings))
+assert len(all_strings_1) == len(all_strings_2)
 
 tokenizer = AutoTokenizer.from_pretrained(TRAINED_MODEL_PATH)
 encode_partial = partial(tokenizer.encode,
@@ -66,7 +81,8 @@ encode_partial = partial(tokenizer.encode,
                          add_special_tokens=True)
 print('Encoding raw strings into model-specific tokens')
 with mp.Pool(MAX_CORES) as p:
-    all_features = np.array(p.map(encode_partial, all_strings))
+    features = [np.array(p.map(encode_partial, all_strings_1)),
+                np.array(p.map(encode_partial, all_strings_2))]
 
 pretrained_config = AutoConfig.from_pretrained(TRAINED_MODEL_PATH,
                                                output_hidden_states=True)
@@ -74,19 +90,7 @@ pretrained_base = AutoModel.from_config(pretrained_config)
 classifier = ClassifierHead(pretrained_base).cuda()
 classifier.load_state_dict(torch.load(os.path.join(TRAINED_MODEL_PATH, WEIGHTS_NAME)))
 
-print('Predicting test comments')
-test_preds = []
-classifier.eval()
-with torch.no_grad():
-    for batch_idx_start in range(0, len(all_features), BATCH_SIZE):
-        batch_idx_end = min(batch_idx_start + BATCH_SIZE, len(all_features))
-        batch_features = torch.tensor(all_features[batch_idx_start:batch_idx_end]).cuda()
-        batch_preds = classifier(batch_features)
-        test_preds.append(batch_preds.cpu())
-
-    test_preds = np.concatenate(test_preds).squeeze()
-
-test_preds = (test_preds[:len(all_strings_2)] + test_preds[len(all_strings_2):]) / 2
+test_preds = np.mean(np.stack([predict(classifier, x) for x in features]), 0)
 
 pd.DataFrame({'id': all_ids, 'toxic': test_preds}).to_csv(SUBMIT_CSV_PATH, index=False)
 print('Elapsed time: {}'.format(time.time() - start_time))
