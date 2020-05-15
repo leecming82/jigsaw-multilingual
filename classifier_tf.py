37dbcdcbf5ba3fb23742e974e4433f9c326820c6
leecming <leecming@gmail.com>
2020-05-15 09:44:58 +0800

    Cleaned up and added a flag for when PT models are used

--- classifier_tf.py
+++ classifier_tf.py
@@ -13,30 +13,30 @@ from preprocessor import get_id_text_label_from_csv, get_id_text_from_test_csv
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 
-RUN_NAME = '9508fr_tf'
+RUN_NAME = '9545es_tf'
 USE_AMP = True
-USE_XLA = False
+USE_XLA = True
 USE_PSEUDO = False  # Add pseudo labels to training dataset
-PRETRAINED_MODEL = 'jplu/tf-flaubert-base-cased'
+FROM_PT = True  # Need to set a flag in TFAutoModel if the pretrained model is a torch model
+USE_VAL_LANG = 'es'  # if set to ISO lang str (e.g., "es") - only pulls that language's validation samples
+PRETRAINED_MODEL = 'mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es'
 TRAIN_SAMPLE_FRAC = 1.  # what % of training data to use
-TRAIN_CSV_PATH = 'data/fr_all.csv'
-TEST_CSV_PATH = 'data/fr_test.csv'
-PSEUDO_CSV_PATH = 'data/submissions/test9452.csv'
+TRAIN_CSV_PATH = 'data/es_all.csv'
+TEST_CSV_PATH = 'data/es_test.csv'
 VAL_CSV_PATH = 'data/validation.csv'
-NUM_GPUS = 2
 MAX_CORES = 24  # limit MP calls to use this # cores at most
 NUM_OUTPUTS = 1
 MAX_SEQ_LEN = 200  # max sequence length for input strings: gets padded/truncated
-NUM_EPOCHS = 6
+NUM_EPOCHS = 4
 BATCH_SIZE = 64
 LR = 1e-5
 
-# os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 tf.autograph.set_verbosity(0)
 tf.config.optimizer.set_jit(USE_XLA)
-# tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
+tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
 
 
 def build_classifier_model():
@@ -49,7 +49,7 @@ def build_classifier_model():
     input = layers.Input(shape=(MAX_SEQ_LEN,), dtype=np.int32)
     pretrained_config = AutoConfig.from_pretrained(PRETRAINED_MODEL,
                                                    output_hidden_states=True)
-    pretrained_base = TFAutoModel.from_pretrained(PRETRAINED_MODEL, config=pretrained_config)
+    pretrained_base = TFAutoModel.from_pretrained(PRETRAINED_MODEL, config=pretrained_config, from_pt=FROM_PT)
     hidden_layer = pretrained_base(input)[0]
 
     # base_output = layers.Conv1D(NUM_OUTPUTS, 1)(hidden_layer)
@@ -68,13 +68,13 @@ def main_driver(train_tuple, val_tuple, test_tuple):
     val_features, val_labels, _ = val_tuple
     test_features, test_ids = test_tuple
 
-    mirrored_strategy = tf.distribute.MirroredStrategy()
-    with mirrored_strategy.scope():
-        classifier = build_classifier_model()
-        opt = Adam(lr=LR)
-        if USE_AMP:
-            opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt)
-        classifier.compile(optimizer=opt, loss='binary_crossentropy')
+    # mirrored_strategy = tf.distribute.MirroredStrategy()
+    # with mirrored_strategy.scope():
+    classifier = build_classifier_model()
+    opt = Adam(lr=LR)
+    if USE_AMP:
+        opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt)
+    classifier.compile(optimizer=opt, loss='binary_crossentropy')
 
     epoch_eval_score = []
     for curr_epoch in range(NUM_EPOCHS):
@@ -82,10 +82,10 @@ def main_driver(train_tuple, val_tuple, test_tuple):
                        batch_size=BATCH_SIZE,
                        epochs=1,
                        verbose=1)
-        # val_preds = classifier.predict(val_features)
-        # val_roc_auc_score = roc_auc_score(val_labels, val_preds)
-        # epoch_eval_score.append(val_roc_auc_score)
-        # print('Epoch {} eval score: {}'.format(curr_epoch, val_roc_auc_score))
+        val_preds = classifier.predict(val_features)
+        val_roc_auc_score = roc_auc_score(val_labels, val_preds)
+        epoch_eval_score.append(val_roc_auc_score)
+        print('Epoch {} eval score: {}'.format(curr_epoch, val_roc_auc_score))
 
         test_preds = classifier.predict(test_features).squeeze()
         pd.DataFrame({'id': test_ids, 'toxic': test_preds}).to_csv('data/outputs/test/{}_{}.csv'.format(RUN_NAME,
@@ -112,7 +112,8 @@ if __name__ == '__main__':
     train_strings = [cln(x) for x in train_strings]
     print(train_strings[0])
 
-    val_ids, val_strings, val_labels = get_id_text_label_from_csv(VAL_CSV_PATH, text_col='comment_text', lang='fr')
+    val_ids, val_strings, val_labels = get_id_text_label_from_csv(VAL_CSV_PATH, text_col='comment_text',
+                                                                  lang=USE_VAL_LANG)
     val_strings = [cln(x) for x in val_strings]
 
     test_ids, test_strings = get_id_text_from_test_csv(TEST_CSV_PATH, text_col='comment_text')
